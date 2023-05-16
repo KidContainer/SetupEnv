fd2181b3d9136252696da8e9cb328ed9ef64497b
Kie <Kidsunbo@users.noreply.github.com>
2023-05-16 18:59:21 +0800

    Create setup_env.sh

---
+++ setup_env.sh
@@ -0,0 +1,24 @@
+# install needed tools
+apt update
+apt install wget curl
+
+# install C++, Python3, Java, Golang from default apt source
+apt install g++ gdb cmake git python3 python3-pip default-jdk golang
+
+# install nodejs
+curl -fsSL https://deb.nodesource.com/setup_20.x | bash - && apt-get install -y nodejs
+
+# install C#
+declare repo_version=$(if command -v lsb_release &> /dev/null; then lsb_release -r -s; else grep -oP '(?<=^VERSION_ID=).+' /etc/os-release | tr -d '"'; fi)
+wget https://packages.microsoft.com/config/ubuntu/$repo_version/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
+apt update
+apt install dotnet-sdk-7.0
+
+# install Dart
+apt-get install apt-transport-https
+wget -qO- https://dl-ssl.google.com/linux/linux_signing_key.pub | gpg --dearmor -o /usr/share/keyrings/dart.gpg
+echo 'deb [signed-by=/usr/share/keyrings/dart.gpg arch=amd64] https://storage.googleapis.com/download.dartlang.org/linux/debian stable main' | tee /etc/apt/sources.list.d/dart_stable.list
+apt-get update
+apt-get install dart
