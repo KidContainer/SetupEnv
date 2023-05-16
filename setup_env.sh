c3fc985eaa650bfdaf17499ea4ddc6c8911e9c66
Kie <Kidsunbo@users.noreply.github.com>
2023-05-16 19:04:47 +0800

    Update setup_env.sh

--- setup_env.sh
+++ setup_env.sh
@@ -3,7 +3,7 @@ apt update
 apt install wget curl
 
 # install C++, Python3, Java, Golang from default apt source
-apt install g++ gdb cmake git python3 python3-pip default-jdk golang
+apt install -y g++ gdb cmake git python3 python3-pip default-jdk golang
 
 # install nodejs
 curl -fsSL https://deb.nodesource.com/setup_20.x | bash - && apt-get install -y nodejs
@@ -14,11 +14,11 @@ wget https://packages.microsoft.com/config/ubuntu/$repo_version/packages-microso
 dpkg -i packages-microsoft-prod.deb
 rm packages-microsoft-prod.deb
 apt update
-apt install dotnet-sdk-7.0
+apt install -y dotnet-sdk-7.0
 
 # install Dart
 apt-get install apt-transport-https
 wget -qO- https://dl-ssl.google.com/linux/linux_signing_key.pub | gpg --dearmor -o /usr/share/keyrings/dart.gpg
 echo 'deb [signed-by=/usr/share/keyrings/dart.gpg arch=amd64] https://storage.googleapis.com/download.dartlang.org/linux/debian stable main' | tee /etc/apt/sources.list.d/dart_stable.list
 apt-get update
-apt-get install dart
+apt-get install -y dart
