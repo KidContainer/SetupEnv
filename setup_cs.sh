982d5a562aec9af73a84aa76e655abec8d6196a6
Kie <Kidsunbo@users.noreply.github.com>
2023-06-13 14:38:33 +0800

    Update setup_cs.sh

--- setup_cs.sh
+++ setup_cs.sh
@@ -4,10 +4,4 @@
 if [ "$1" != "skip_update" ]; then
     apt update
 fi
-apt install -y wget
-declare repo_version=$(if command -v lsb_release &> /dev/null; then lsb_release -r -s; else grep -oP '(?<=^VERSION_ID=).+' /etc/os-release | tr -d '"'; fi)
-wget https://packages.microsoft.com/config/ubuntu/$repo_version/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
-dpkg -i packages-microsoft-prod.deb
-rm packages-microsoft-prod.deb
-apt update
 apt install -y dotnet-sdk-7.0
