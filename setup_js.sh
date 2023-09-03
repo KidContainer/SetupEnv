3eaff538513af2895f057da42f670b0436ed55e2
Kie <Kidsunbo@users.noreply.github.com>
2023-09-03 17:17:03 +0800

    Update setup_js.sh

--- setup_js.sh
+++ setup_js.sh
@@ -4,5 +4,8 @@
 if [ "$1" != "skip_update" ]; then
     apt update
 fi
-apt install -y curl
-curl -fsSL https://deb.nodesource.com/setup_20.x | bash - && apt-get install -y nodejs
+apt install -y ca-certificates curl gnupg
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+NODE_MAJOR=20
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
+apt-get update && apt-get install nodejs -y
